bdd7967a696a0854bda17d1a05d4cc2f96ad0ec2
xopose <lazarevds2001@gmail.com>
2025-05-01 00:29:26 +0300

    fix

---
+++ backend/src/test/java/com/lazer/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
