74bce7637c067ae0776fe496900bd895e30e75aa
lazer <lazarevds2001@gmail.com>
2025-06-26 21:32:41 +0300

    добавил бота

--- backend/src/main/java/com/lazer/backend/service/BotRest.java
+++ backend/src/main/java/com/lazer/backend/service/BotRest.java
@@ -3,23 +3,28 @@ package com.lazer.backend.service;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class BotRest {
     public static void send(String message) {
-        String url = "http://telegram-bot-service:41401/";
-        Map<String, String> requestBody = Map.of("message", message);
+        try{
+            String url = "http://telegram-bot-service:41401/";
+            Map<String, String> requestBody = Map.of("message", message);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
+            HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, headers);
 
-        // Отправка запроса
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.postForObject(url, request, String.class);
+            // Отправка запроса
+            RestTemplate restTemplate = new RestTemplate();
+            String response = restTemplate.postForObject(url, request, String.class);
+        }
+        catch (RestClientException e) {
+            System.out.println("can`t send data");
+        }
     }
 }
